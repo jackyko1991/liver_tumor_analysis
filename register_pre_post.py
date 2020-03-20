c26b73c657773bbaea22e47a3eb7b06efc3505ec
Ka Long Ko <jackkykokoko@gmail.com>
2020-03-20 18:20:47 +0800

    add majority voting

--- register_pre_post.py
+++ register_pre_post.py
@@ -1,11 +1,60 @@
 import SimpleITK as sitk
 import os
+import shutil
 
 def main():
 	data_dir = "./data/by_case"
+	executable = "F:/liver_tumor/binary/ExpertAutomatedRegistration.exe"
+	resample_exe = "F:/liver_tumor/binary/ResampleScalarVectorDWIVolume.exe"
 
-	for case in os.listdir(data_dir):
-		linear_tfm = sitk.ReadTransform("D:/projects/liver_tumor/data/by_case/ChoySimWang")
+	ignore = []
+	phases = ["HA","PV"]
+	images = ["temporal_mIP.nii.gz","temporal_mIP_1-3.nii.gz","temporal_mIP_4-7.nii.gz","temporal_mIP_8-10.nii.gz","01.nii.gz"]
+
+	for patient in os.listdir(data_dir):
+		if patient in ignore:
+			continue
+
+		if not os.path.exists(os.path.join(data_dir,patient,"LinearTransform.tfm")):
+			continue
+
+		if os.path.isdir(os.path.join(data_dir, patient)):
+			print("Working on",patient)
+			
+			for phase in phases:
+				# clear target directory
+				# shutil.rmtree(os.path.join(data_dir,patient,"post","nii_reg_pre",phase),ignore_errors=True)
+				os.makedirs(os.path.join(data_dir,patient,"post","nii_reg_pre",phase),exist_ok=True)
+				for image in images:
+
+					# registration 
+					for image in images:
+						fixed = os.path.join(data_dir,patient,"pre","nii_reg","HA","temporal_mIP.nii.gz")
+						moving = os.path.join(data_dir,patient,"post","nii_reg",phase,image)
+						resample = os.path.join(data_dir,patient,"post","nii_reg_pre",phase,image)
+						initialization = os.path.join(data_dir,patient,"LinearTransform.tfm")
+						tfm = os.path.join(data_dir,patient,"BSplineTransform.tfm")
+
+						if image == "temporal_mIP.nii.gz":
+							command = executable + " " + \
+								"--registration BSpline " + \
+								"--initialization None " + \
+								"--loadTransform " + initialization + " " +\
+								"--saveTransform " + tfm + " "+\
+								"--resampledImage " + resample + " "+\
+								fixed + " " +\
+								moving
+						else:
+							command = executable + " " + \
+								"--registration BSpline " + \
+								"--initialization None " + \
+								"--loadTransform " + initialization + " " +\
+								"--resampledImage " + resample + " "+\
+								fixed + " " +\
+								moving
+						os.system(command)
+
+					# exit()
 
 if __name__ == "__main__":
-	main():
+	main()
---
+++ segmentation_average.py
@@ -0,0 +1,38 @@
+import SimpleITK as sitk
+import numpy as np
+import os
+
+def main():
+	data_dir = "./data/by_case"
+
+	stages = ["pre","post"]
+	phases = ["HA","PV"]
+
+	for case in os.listdir(data_dir):
+		print(case)
+		images = []
+		images_np = []
+		for stage in stages:
+			for phase in phases:
+				if stage == "pre":
+					filename = os.path.join(data_dir,case, stage,"nii_reg",phase,"label_tf.nii.gz")
+				else:
+					filename = os.path.join(data_dir,case, stage,"nii_reg_pre",phase,"label_tf.nii.gz")
+
+				if not os.path.exists(filename):
+					continue
+				reader = sitk.ImageFileReader()
+				reader.SetFileName(filename)
+				castFilter = sitk.CastImageFilter()
+				castFilter.SetOutputPixelType(sitk.sitkUInt8)
+				images.append(castFilter.Execute(reader.Execute()))
+
+		votingFilter = sitk.LabelVotingImageFilter()
+		votingFilter.SetLabelForUndecidedPixels(0)
+		label = votingFilter.Execute(images)
+		writer = sitk.ImageFileWriter()
+		writer.SetFileName(os.path.join(data_dir,case, "label.nii.gz"))
+		writer.Execute(label)
+
+if __name__ == "__main__":
+	main()
